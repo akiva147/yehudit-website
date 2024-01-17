280ae857a9ee1814aa126369dd99c6c628e0f283
Akiva levi <levi.akiva@agamimp.net>
2024-01-17 09:28:47 +0200

    feat(style,AboutPAge): added content to the about page and added interactivity for the links and buttons

--- src/pages/AboutPage/AboutPage.tsx
+++ src/pages/AboutPage/AboutPage.tsx
@@ -1,7 +1,31 @@
+import { LoadingImage } from '@/components/LoadingImage'
 import classes from './about-page.module.scss'
 
 export interface AboutPageProps {}
 
 export const AboutPage = (props: AboutPageProps) => {
-    return <div>About Page (:</div>
+    return (
+        <div className={classes.container}>
+            <header>
+                <h1>About Me</h1>
+            </header>
+            <section className={classes.aboutMe}>
+                <div className={classes.text}>
+                    <p>
+                        I have over two years of experience building UX and UI
+                        design for variety of websites. I have a high motivation
+                        and glad to take up challenges and solve complicated
+                        problems.
+                    </p>
+                    <span>
+                        <a href="mailto:yehuditlevi.66@gmail.com">
+                            yehuditlevi.66@gmail.com
+                        </a>
+                        <a href="tel:+972542572119">+972 54 257 2119</a>
+                    </span>
+                </div>
+                <LoadingImage isLoading={false} src="" alt="" />
+            </section>
+        </div>
+    )
 }
